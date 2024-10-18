fb8a3c2cb2479333c63f321ad9fa0f0d985f1cb2
emanuela <emanuela.markovic@hotmail.com>
2024-10-18 09:02:11 +0200

    Add global styles and integrate Navbar in App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Montserrat } from "next/font/google";
+import Navbar from "@/components/NavBar";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Webdeveloper Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+      <footer className="flex gap-6 flex-wrap items-center justify-center"></footer>
+    </>
+  );
 }
