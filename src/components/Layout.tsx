1a41e121afed8f79eb01c8a910175a2f01066e5a
emanuela <emanuela.markovic@hotmail.com>
2024-10-18 20:41:29 +0200

    Refactor Layout component to accept children and apply custom styles.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout = ({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={`w-full inline-block z-0 bg-light p-32 ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
