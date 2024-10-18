236aec59e98347c47514df89e50a9a3984fd4cfd
emanuela <emanuela.markovic@hotmail.com>
2024-10-18 20:42:55 +0200

    Update Home component layout and add profile image.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import Layout from "@/components/Layout";
 import Head from "next/head";
+import Image from "next/image";
+import profilePicture from "../../public/Images/profilePicture.png";
+import AnimatedText from "@/components/AnimatedText";
 
 export default function Home() {
   return (
@@ -8,8 +12,30 @@ export default function Home() {
         <meta name="description" content="Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello world!</h1>
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Layout className="pt-0">
+          <div className="flex items-center justify-between w-full">
+            <div className="w-1/2">
+              <Image
+                src={profilePicture}
+                alt="emanuelamarkovic"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-4xl"
+              />
+              <p className="my-4 text-base font-medium">
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   );
