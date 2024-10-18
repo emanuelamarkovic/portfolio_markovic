bc9e41315915b65dc1eca756a163e52ca493bdc4
emanuela <emanuela.markovic@hotmail.com>
2024-10-18 20:42:01 +0200

    Fix typo in animation property and enhance capitalization logic.

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const quote = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      steggerChildren: 0.08,
+    },
+  },
+};
+
+const singleWord = {
+  initial: { opacity: 0, y: 50 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1 },
+  },
+};
+
+interface AnimatedTextProps {
+  text: string;
+  className?: string;
+}
+
+const AnimatedText: React.FC<AnimatedTextProps> = ({
+  text,
+  className = "",
+}) => {
+  const capitalizeWords = (str: string) => {
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  const capitalizedText = capitalizeWords(text);
+
+  return (
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+      <motion.h1
+        className={`inline-block w-full text-dark font-bold text-8xl ${className}`}
+        variants={quote}
+        // initial="initial"
+        // animate="animate"
+      >
+        {capitalizedText.split("").map((char, index) => (
+          <motion.span
+            key={char + "-" + index}
+            className="inline-block"
+            variants={singleWord}
+            initial="initial"
+            animate="animate"
+          >
+            {char}&nbsp;
+          </motion.span>
+        ))}
+      </motion.h1>
+    </div>
+  );
+};
+
+export default AnimatedText;
