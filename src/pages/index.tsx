df887d162c022bb38ad0f26cb2aad9dfb0df0e54
emanuela <emanuela.markovic@hotmail.com>
2024-10-25 13:46:29 +0200

    fix: Updated text size to 5xl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="!text-4xl"
+                className="!text-5xl"
               />
               <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
