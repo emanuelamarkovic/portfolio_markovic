f2b04661095b419356318ab6c39c17a3b741b3f7
emanuela <emanuela.markovic@hotmail.com>
2024-10-25 13:43:16 +0200

    feat: Update text styling for improved readability

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -42,7 +42,8 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({
   return (
     <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold text-8xl ${className}`}
+        className={`inline-block w-full text-dark font-bold text-7xl ${className}`}
+        style={{ letterSpacing: "-0.1em" }}
         variants={quote}
         // initial="initial"
         // animate="animate"
