521a64a255bd20d6f71620d20f0a67ceb20f4b36
emanuela <emanuela.markovic@hotmail.com>
2024-11-04 15:48:50 +0100

    Update: primary color in Tailwind config to new yellow shade

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
-        primary: "#B63E96",
+        primary: "#fcd34d",
         primaryDark: "#58E6D9",
       },
       fontFamily: {
